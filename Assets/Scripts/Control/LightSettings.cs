b8a5076ae5f270022b1e68e4cd704683a3a3a419
Irina Ishchenko <irina@uken.com>
2019-01-29 23:10:40 -0500

    Added Day/Night rotation script

---
+++ Assets/Scripts/Control/LightSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSettings : MonoBehaviour {
+
+    [SerializeField] Color Sky, Equator, Ground, SunColor;
+    [SerializeField] float RotateSpeed;
+    Light Sun;
+
+    private void Start()
+    {
+        Sun = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        RenderSettings.ambientSkyColor = Sky;
+        RenderSettings.ambientGroundColor = Ground;
+        RenderSettings.ambientEquatorColor = Equator;
+        Sun.color = SunColor;
+        transform.Rotate(transform.right, RotateSpeed, Space.Self);
+    }
+}
